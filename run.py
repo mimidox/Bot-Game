9d80c5cb2da8e98be409989f62462013d25ddd89
Vivi Echave <xnoahruizz@gmail.com>
2026-02-20 22:13:22 -0400

    Bot not totally ready for use

---
+++ run.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from threading import Thread
+from highrise.__main__ import *
+import time
+
+
+class WebServer():
+
+  def __init__(self):
+    self.app = Flask(__name__)
+
+    @self.app.route('/')
+    def index() -> str:
+      return "Funcionando"
+
+  def run(self) -> None:
+    self.app.run(host='0.0.0.0', port=8084)
+
+  def keep_alive(self):
+    t = Thread(target=self.run)
+    t.start()
+
+
+class RunBot():
+  room_id = "67cb91fc6fadc0155d00a2ac"
+  bot_token = "54972b51e96400db923fd09a3167cb9f63f8529aedd105e55b879331ae0c5cf0"
+
+  bot_file = "main"
+  bot_class = "Bot"
+
+  def __init__(self) -> None:
+    self.definitions = [
+        BotDefinition(
+            getattr(import_module(self.bot_file), self.bot_class)(),
+            self.room_id, self.bot_token)
+    ]  # More BotDefinition classes can be added to the definitions list
+
+  def run_loop(self) -> None:
+    while True:
+      try:
+        arun(main(self.definitions))
+
+      except Exception as e:
+        print("Error: ", e)
+        time.sleep(5)
+
+if __name__ == "__main__":
+  WebServer().keep_alive()
+
+  RunBot().run_loop()
+  flask.__version__
